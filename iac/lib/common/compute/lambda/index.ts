ec14a666e827cb35a38b4da6b077d80ad28bdb36
Quentin Roy-Foster <quentinrf@gmail.com>
2024-07-14 13:35:03 -0400

    Implement basic extended Lambda Construct
    to provide some default configuration and allow for extensibility of the
    resource moving forward

---
+++ iac/lib/common/compute/lambda/index.ts
@@ -0,0 +1,28 @@
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cdk from 'aws-cdk-lib';
+
+interface LambdaProps extends lambda.FunctionProps {}
+/*
+Extended Lambda class which provides some default configuration
+and allows for customizability in future
+*/
+export class Lambda extends lambda.Function {
+    
+    constructor(scope: cdk.Stack, id: string, props: LambdaProps){
+        super(scope, id, {
+            ...props,
+            architecture: lambda.Architecture.ARM_64,
+            timeout: cdk.Duration.seconds(30),
+            logGroup: new logs.LogGroup(scope, id+'-LogGroup', {
+                retention: logs.RetentionDays.TWO_WEEKS,
+                logGroupName: id+'-LogGroup',
+            }),
+            role: new iam.Role(scope, id+'-Role', {
+                assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+            }),
+            loggingFormat: lambda.LoggingFormat.JSON,
+        })
+    }
+}
--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -1,16 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import * as path from 'path';
+import { Lambda } from './common/compute/lambda'
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class IacStack extends cdk.Stack {
+export class CloudCostAuditorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const ruleSetLambda = new Lambda(this, 'RuleSetLambda', {
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      handler: 'main',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../artifacts/ruleSetLambda.zip')),
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'IacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const resourceLambda = new Lambda(this, 'ResourceLambda', {
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      handler: 'main',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler'))
+    });
+
+    const actionLambda = new Lambda(this, 'ActionLambda', {
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      handler: 'main',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler'))
+    });
+
+    
+    
+    
+    
+    
+
+    
   }
 }
